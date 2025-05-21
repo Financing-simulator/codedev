2e2995005d0d8f79ab3713781b819daadf580c43
Adilson krischanski <adilson.krischanski@outlook.com>
2025-05-21 09:46:17 -0300

    inital desing screen

--- codeDev/src/app/app.component.ts
+++ codeDev/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TopMenuComponent } from './layout/top-menu/top-menu.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,TopMenuComponent,SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
